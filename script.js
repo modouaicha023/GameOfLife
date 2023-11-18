7eecaccb039c716b7966c407943cb161a6471190
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-11-18 20:06:42 +0000

    refacoting  the code and and change row and cols

--- script.js
+++ script.js
@@ -1,93 +1,17 @@
 const gridContainer = document.querySelector('.grid');
-
-let rows = 100;
-let cols = 100;
-
-function initMatrix() {
-    return Array.from({ length: rows }, () => Array(cols).fill(0));
-}
-
-gridContainer.style.gridTemplateColumns = `repeat(${cols ? cols : 100}, 20px)`;
-gridContainer.style.gridTemplateRows = `repeat(${rows ? rows : 100}, 20px)`;
-
-
-let cellsMatrix = initMatrix();
-cellsMatrix[4][9] = 1;
-cellsMatrix[4][10] = 1;
-cellsMatrix[4][11] = 1;
-cellsMatrix[3][11] = 1;
-cellsMatrix[2][10] = 1;
-cellsMatrix[8][7] = 1;
-cellsMatrix[8][5] = 1;
-cellsMatrix[8][6] = 1;
-let allCells = [];
-
-createCellsElements();
-
-
-function createCellsElements() {
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const cell = document.createElement('div');
-            cell.className = 'cell';
-            cell.id = `${i}-${j}`;
-            gridContainer.appendChild(cell);
-            allCells.push(cell);
-        }
-    }
-}
-
-updateCellClasses();
-
-allCells.forEach((cell) => {
-    cell.addEventListener("click", () => handleCellClick(cell));
-});
-
-function handleCellClick(cell) {
-    const idCell = cell.id;
-    const cellRow = Number(idCell.split("-")[0]);
-    const cellCol = Number(idCell.split("-")[1]);
-    cellsMatrix[cellRow][cellCol] = 1 - cellsMatrix[cellRow][cellCol];
-    updateCellClasses();
-}
-
-function getAliveCellNeighbors(row, col) {
-    let neighbors = [];
-
-    for (let i = row - 1; i <= row + 1; i++) {
-        for (let j = col - 1; j <= col + 1; j++) {
-            if (i >= 0 && i < cellsMatrix.length && j >= 0 && j < cellsMatrix[0].length) {
-                if (!(i === row && j === col) && cellsMatrix[i][j] === 1) {
-                    neighbors = [...neighbors, { "row": i, "col": j }];
-                }
-            }
-        }
-    }
-
-    return neighbors;
-}
-
+const btnPlay = document.getElementById("play");
+const btnStop = document.getElementById("stop");
+const btnReset = document.getElementById("reset");
+const inputSpeed = document.getElementById("speed");
+const inputRows = document.getElementById("rows");
+const inputColunms = document.getElementById("columns");
+let interval = inputSpeed.value * 1000;
+let intervalId;
 let generation = 0;
-
-function UpdateCell() {
-    const updatedMatrix = initMatrix();
-    cellsMatrix.forEach((row, i) => {
-        row.forEach((col, j) => {
-            const numNeighborsAlive = getAliveCellNeighbors(i, j).length;
-
-            if (cellsMatrix[i][j] === 0 && numNeighborsAlive === 3) {
-                updatedMatrix[i][j] = 1;
-            } else if (cellsMatrix[i][j] === 1 && (numNeighborsAlive === 3 || numNeighborsAlive === 2)) {
-                updatedMatrix[i][j] = 1;
-            } else if (numNeighborsAlive < 2 || numNeighborsAlive > 3) {
-                updatedMatrix[i][j] = 0;
-            }
-        });
-    });
-
-    cellsMatrix = updatedMatrix;
-    updateCellClasses();
-    generation++;
+let cellsMatrix = [];
+let allCells = [];
+function initMatrix(initRows, initCols) {
+    return Array.from({ length: initRows }, () => Array.from({ length: initCols }).fill(0));
 }
 
 function updateCellClasses() {
@@ -99,5 +23,153 @@ function updateCellClasses() {
     });
 }
 
-let interval = .1 * 1000; // 1 second
-setInterval(UpdateCell, interval);
+function createCellsElements(rows, cols, allCells) {
+    gridContainer.innerHTML = "";
+    // allCells = [];
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.id = `${i}-${j}`;
+            gridContainer.appendChild(cell);
+            allCells.push(cell);
+        }
+    }
+}
+
+function newRound(r, l) {
+
+
+
+
+    // Step1 is to initialize the matrix with each case the value 0 
+    cellsMatrix = initMatrix(inputRows.value, inputColunms.value);
+
+    //Step2 is to create the cells elements in the dom with the length of this matrix
+    createCellsElements(inputRows.value, inputColunms.value, allCells);
+
+    // Step3 is update the  width and height of the  gridContainer element
+    gridContainer.style.gridTemplateRows = `repeat(${inputRows.value}, 20px)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${inputColunms.value}, 20px)`;
+
+    // set alive some cell
+    function setRandomAliveCell(Nrows, Ncolumns) {
+        for (let index = 0; index < Nrows * Ncolumns; index++) {
+            const randomRow = Math.floor(Math.random() * Nrows);
+            const randomCol = Math.floor(Math.random() * Ncolumns);
+            cellsMatrix[randomRow][randomCol] = 1;
+        }
+    }
+    setRandomAliveCell(inputRows.value, inputColunms.value);
+    updateCellClasses();
+
+
+    function handleCellClick(cell) {
+        const idCell = cell.id;
+        const cellRow = Number(idCell.split("-")[0]);
+        const cellCol = Number(idCell.split("-")[1]);
+        cell.classList.toggle("alive");
+        cellsMatrix[cellRow][cellCol] = 1 - cellsMatrix[cellRow][cellCol];
+    }
+
+    allCells.forEach((cell) => {
+        cell.addEventListener("click", () => handleCellClick(cell));
+    });
+
+    function getAliveCellNeighbors(row, col) {
+        let neighbors = [];
+
+        for (let i = row - 1; i <= row + 1; i++) {
+            for (let j = col - 1; j <= col + 1; j++) {
+                if (i >= 0 && i < cellsMatrix.length && j >= 0 && j < cellsMatrix[0].length) {
+                    if (!(i === row && j === col) && cellsMatrix[i][j] === 1) {
+                        neighbors = [...neighbors, { "row": i, "col": j }];
+                    }
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    function UpdateCell() {
+        const numRows = cellsMatrix.length;
+        const numCols = cellsMatrix[0].length;
+
+        const updatedMatrix = initMatrix(numRows, numCols);
+
+        cellsMatrix.forEach((row, i) => {
+            row.forEach((col, j) => {
+                const numNeighborsAlive = getAliveCellNeighbors(i, j).length;
+
+                if (cellsMatrix[i][j] === 0 && numNeighborsAlive === 3) {
+                    updatedMatrix[i][j] = 1;
+                } else if (cellsMatrix[i][j] === 1 && (numNeighborsAlive === 3 || numNeighborsAlive === 2)) {
+                    updatedMatrix[i][j] = 1;
+                } else {
+                    updatedMatrix[i][j] = 0;
+                }
+            });
+        });
+
+        cellsMatrix = updatedMatrix;
+        updateCellClasses();
+        generation++;
+    }
+
+
+    function startGame() {
+        if (!intervalId) {
+            intervalId = setInterval(UpdateCell, interval);
+        }
+        console.log("Game on play");
+    }
+
+    function stopGame() {
+        clearInterval(intervalId);
+        intervalId = null;
+        console.log("Game Stopped");
+    }
+    function resetGame() {
+        stopGame();
+        cellsMatrix = initMatrix(10, 10);
+        updateCellClasses()
+
+        // window.location.reload();
+    }
+
+    btnPlay.addEventListener("click", startGame);
+    btnStop.addEventListener("click", stopGame);
+    btnReset.addEventListener("click", resetGame);
+}
+
+
+
+inputRows.addEventListener("change", () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    newRound(inputRows.value, inputColunms.value);
+
+
+});
+inputColunms.addEventListener("change", () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    newRound(inputRows.value, inputColunms.value);
+
+});
+
+
+inputSpeed.addEventListener("change", () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    const speedValue = parseFloat(inputSpeed.value);
+    if (!isNaN(speedValue) && speedValue > 0) {
+        interval = speedValue * 1000;
+    }
+});
+
+window.addEventListener('load', function () {
+    newRound(inputRows.value, inputColunms.value);
+    updateCellClasses()
+});
--- style.js
+++ style.js
@@ -1,93 +1,93 @@
-const body = document.getElementById('body');
+// const body = document.getElementById('body');
 
-let rowsStyle = 100;
-let colsStyle = 100;
+// let rowsStyle = 100;
+// let colsStyle = 100;
 
-function initMatrixStyle() {
-    return Array.from({ length: rowsStyle }, () => Array(colsStyle).fill(0));
-}
+// function initMatrixStyle() {
+//     return Array.from({ length: rowsStyle }, () => Array(colsStyle).fill(0));
+// }
 
-body.style.gridTemplateColumns = `repeat(${colsStyle ? colsStyle : 100}, 50px)`;
-body.style.gridTemplateRows = `repeat(${rowsStyle ? rowsStyle : 100}, 50px)`;
-
-
-let cellsMatrixStyle = initMatrixStyle();
-setAliveSomeCell();
-setInterval(setAliveSomeCell,2000)
-function setAliveSomeCell() {
-    for (let index = 0; index < rowsStyle*colsStyle; index++) {
-        const r = Math.floor(Math.random() * rowsStyle);
-        const c = Math.floor(Math.random() * colsStyle);
-        cellsMatrixStyle[r][c] = 1;
-    }
-}
-
-let allCellsStyle = [];
+// body.style.gridTemplateColumns = `repeat(${colsStyle ? colsStyle : 100}, 50px)`;
+// body.style.gridTemplateRows = `repeat(${rowsStyle ? rowsStyle : 100}, 50px)`;
 
 
-createCellsElementsStyle();
+// let cellsMatrixStyle = initMatrixStyle();
+// setAliveSomeCell();
+// setInterval(setAliveSomeCell,2000)
+// function setAliveSomeCell() {
+//     for (let index = 0; index < rowsStyle*colsStyle; index++) {
+//         const r = Math.floor(Math.random() * rowsStyle);
+//         const c = Math.floor(Math.random() * colsStyle);
+//         cellsMatrixStyle[r][c] = 1;
+//     }
+// }
+
+// let allCellsStyle = [];
 
 
-function createCellsElementsStyle() {
-    for (let i = 0; i < rowsStyle; i++) {
-        for (let j = 0; j < colsStyle; j++) {
-            const cellStyle = document.createElement('div');
-            cellStyle.className = 'cellStyle';
-            cellStyle.id = `style-${i}-${j}`;
-            body.appendChild(cellStyle);
-            allCellsStyle.push(cellStyle);
-        }
-    }
-}
-
-updateCellClassStyle();
+// createCellsElementsStyle();
 
 
-function getAliveCellNeighborsStyle(row, col) {
-    let neighborsStyle = [];
+// function createCellsElementsStyle() {
+//     for (let i = 0; i < rowsStyle; i++) {
+//         for (let j = 0; j < colsStyle; j++) {
+//             const cellStyle = document.createElement('div');
+//             cellStyle.className = 'cellStyle';
+//             cellStyle.id = `style-${i}-${j}`;
+//             body.appendChild(cellStyle);
+//             allCellsStyle.push(cellStyle);
+//         }
+//     }
+// }
 
-    for (let i = row - 1; i <= row + 1; i++) {
-        for (let j = col - 1; j <= col + 1; j++) {
-            if (i >= 0 && i < cellsMatrixStyle.length && j >= 0 && j < cellsMatrixStyle[0].length) {
-                if (!(i === row && j === col) && cellsMatrixStyle[i][j] === 1) {
-                    neighborsStyle = [...neighborsStyle, { "row": i, "col": j }];
-                }
-            }
-        }
-    }
-
-    return neighborsStyle;
-}
+// updateCellClassStyle();
 
 
-function UpdateCellStyle() {
-    const updatedMatrixStyle = initMatrixStyle();
-    cellsMatrixStyle.forEach((row, i) => {
-        row.forEach((col, j) => {
-            const numNeighborsAliveStyle = getAliveCellNeighborsStyle(i, j).length;
+// function getAliveCellNeighborsStyle(row, col) {
+//     let neighborsStyle = [];
 
-            if (cellsMatrixStyle[i][j] === 0 && numNeighborsAliveStyle === 3) {
-                updatedMatrixStyle[i][j] = 1;
-            } else if (cellsMatrixStyle[i][j] === 1 && (numNeighborsAliveStyle === 3 || numNeighborsAliveStyle === 2)) {
-                updatedMatrixStyle[i][j] = 1;
-            } else if (numNeighborsAliveStyle < 2 || numNeighborsAliveStyle > 3) {
-                updatedMatrixStyle[i][j] = 0;
-            }
-        });
-    });
+//     for (let i = row - 1; i <= row + 1; i++) {
+//         for (let j = col - 1; j <= col + 1; j++) {
+//             if (i >= 0 && i < cellsMatrixStyle.length && j >= 0 && j < cellsMatrixStyle[0].length) {
+//                 if (!(i === row && j === col) && cellsMatrixStyle[i][j] === 1) {
+//                     neighborsStyle = [...neighborsStyle, { "row": i, "col": j }];
+//                 }
+//             }
+//         }
+//     }
 
-    cellsMatrixStyle = updatedMatrixStyle;
-    updateCellClassStyle();
-}
+//     return neighborsStyle;
+// }
 
-function updateCellClassStyle() {
-    allCellsStyle.forEach((cellStyle) => {
-        const idCellStyle = cellStyle.id;
-        const cellRowStyle = Number(idCellStyle.split("-")[1]);
-        const cellColStyle = Number(idCellStyle.split("-")[2]);
-        cellStyle.classList.toggle("aliveStyle", cellsMatrixStyle[cellRowStyle][cellColStyle] === 1);
-    });
-}
 
-let intervalStyle = .5 * 1000; // 1 second
-setInterval(UpdateCellStyle, intervalStyle);
+// function UpdateCellStyle() {
+//     const updatedMatrixStyle = initMatrixStyle();
+//     cellsMatrixStyle.forEach((row, i) => {
+//         row.forEach((col, j) => {
+//             const numNeighborsAliveStyle = getAliveCellNeighborsStyle(i, j).length;
+
+//             if (cellsMatrixStyle[i][j] === 0 && numNeighborsAliveStyle === 3) {
+//                 updatedMatrixStyle[i][j] = 1;
+//             } else if (cellsMatrixStyle[i][j] === 1 && (numNeighborsAliveStyle === 3 || numNeighborsAliveStyle === 2)) {
+//                 updatedMatrixStyle[i][j] = 1;
+//             } else if (numNeighborsAliveStyle < 2 || numNeighborsAliveStyle > 3) {
+//                 updatedMatrixStyle[i][j] = 0;
+//             }
+//         });
+//     });
+
+//     cellsMatrixStyle = updatedMatrixStyle;
+//     updateCellClassStyle();
+// }
+
+// function updateCellClassStyle() {
+//     allCellsStyle.forEach((cellStyle) => {
+//         const idCellStyle = cellStyle.id;
+//         const cellRowStyle = Number(idCellStyle.split("-")[1]);
+//         const cellColStyle = Number(idCellStyle.split("-")[2]);
+//         cellStyle.classList.toggle("aliveStyle", cellsMatrixStyle[cellRowStyle][cellColStyle] === 1);
+//     });
+// }
+
+// let intervalStyle = .5 * 1000; // 1 second
+// setInterval(UpdateCellStyle, intervalStyle);
