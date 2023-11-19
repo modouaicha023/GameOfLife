81967aa77e6e5e22eb6530d0fb5fae0b36fbb380
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-11-19 19:17:24 +0000

    stop the  animation backgroundForBetterPerformance

--- headerTextStyle.js
+++ headerTextStyle.js
@@ -210,5 +210,5 @@ function updateCellClassStyleText() {
     });
 }
 
-let intervalStyleText = 10 * 1000; // 1 second
-setInterval(UpdateCellStyleText, intervalStyleText);
+// let intervalStyleText = 10 * 1000; // 1 second
+// setInterval(UpdateCellStyleText, intervalStyleText);
